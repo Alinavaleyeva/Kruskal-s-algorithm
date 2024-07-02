83df5b8bec9e07b069a5dafa213017a2447c94cc
Alinavaleyeva <alina.valeyeva21@yandex.ru>
2024-07-02 19:50:26 +0300

    Graph structure and Kruskal`s algorithm

---
+++ src/main/java/com/kruskal/algorithm/StepMessage.java
@@ -0,0 +1,47 @@
+package com.kruskal.algorithm;
+
+import com.kruskal.graph.EdgeData;
+
+public class StepMessage {
+    private final StepMessageType messageType;
+    private final int firstNodeId;
+    private final int secondNodeId;
+    private final int edgeId;
+    private final int weightShift;
+
+    public StepMessage(EdgeData edgeData, StepMessageType messageType, int weightShift){
+        this.weightShift = weightShift;
+        this.messageType = messageType;
+        firstNodeId = edgeData.getFirstNodeId();
+        secondNodeId = edgeData.getSecondNodeId();
+        edgeId = edgeData.getId();
+    }
+
+    public StepMessage(StepMessageType messageType){
+        this.messageType = messageType;
+        firstNodeId = -1;
+        secondNodeId = -1;
+        edgeId = -1;
+        weightShift = -1;
+    }
+
+    public StepMessageType getType(){
+        return messageType;
+    }
+
+    public int getFirstNodeId(){
+        return firstNodeId;
+    }
+
+    public int getEdgeId() {
+        return edgeId;
+    }
+
+    public int getSecondNodeId() {
+        return secondNodeId;
+    }
+
+    int getWeightShift(){
+        return weightShift;
+    }
+}
---
+++ src/main/java/com/kruskal/algorithm/StepMessageType.java
@@ -0,0 +1,7 @@
+package com.kruskal.algorithm;
+
+public enum StepMessageType {
+    EDGEADDED,
+    EDGEDECLINED,
+    ALGORITHMENDED
+}
