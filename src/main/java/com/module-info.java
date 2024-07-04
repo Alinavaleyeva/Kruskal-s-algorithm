6c6a65ae31c7dec783b00f0c20f42332ce50de9b
astya-min <54018364+astya-min@users.noreply.github.com>
2024-07-04 16:02:30 +0300

    Add files via upload

---
+++ src/main/java/com/module-info.java
@@ -0,0 +1,16 @@
+module com.kruskal.kruskalalgorithm {
+    requires javafx.controls;
+    requires javafx.fxml;
+            
+        requires org.controlsfx.controls;
+            requires com.dlsc.formsfx;
+                    requires org.kordamp.bootstrapfx.core;
+            
+    opens com.kruskal.gui to javafx.fxml;
+    exports com.kruskal.gui;
+    exports com.kruskal.shapeview;
+    opens com.kruskal.shapeview to javafx.fxml;
+    exports com.kruskal.kruskalalgorithm;
+    exports com.kruskal.controlstructures;
+    exports com.kruskal.graph;
+}
